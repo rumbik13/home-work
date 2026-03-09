ae5984d72352f6529cc6a1a249248ab03d2687af
rumbik13 <rembobeca@gmail.com>
2026-03-09 20:41:51 +0300

    Add files via upload

--- Phones and gadgets/js/index.js
+++ Phones and gadgets/js/index.js
@@ -3,33 +3,40 @@ import Modal from "./modal.js";
 import { heroSlider } from "./hero-slider.js";
 
 try {
+  const searchInput = document.querySelector(".search-input");
 
-  new BurgerMenu(
-    {
-      BURGER: "burger",
-      BURGER_OPEN: "burger--open",
-      HEADER_MENU: "header__menu",
-      HEADER_MENU_OPEN: "header__menu--open",
-      lABEL: {
-        OPEN: "Открыть меню",
-        CLOSE: "Закрыть меню",
-      },
-      PAGE_BODY: "page__body",
-      PAGE_BODY_NO_SCROLL: "page__body--no-scroll", 
-      MENU_LINK: "menu__link",
-      BREAKPOINT: 768,
-      MAIN: 'main',
+  const tags = document.querySelectorAll(".tag");
+
+  tags.forEach((tag) => {
+    tag.addEventListener("click", () => {
+      searchInput.value = tag.textContent;
+
+      searchInput.focus();
+    });
+  });
+
+  new BurgerMenu({
+    BURGER: "burger",
+    BURGER_OPEN: "burger--open",
+    HEADER_MENU: "header__menu",
+    HEADER_MENU_OPEN: "header__menu--open",
+    lABEL: {
+      OPEN: "Открыть меню",
+      CLOSE: "Закрыть меню",
     },
-  );
+    PAGE_BODY: "page__body",
+    PAGE_BODY_NO_SCROLL: "page__body--no-scroll",
+    MENU_LINK: "menu__link",
+    BREAKPOINT: 768,
+    MAIN: "main",
+  });
 
-  new Modal(
-    {
-      PAGE_BODY: "page__body",
-      PAGE_BODY_NO_SCROLL: "page__body--no-scroll", 
-    }
-  )
+  new Modal({
+    PAGE_BODY: "page__body",
+    PAGE_BODY_NO_SCROLL: "page__body--no-scroll",
+  });
 
-    heroSlider(); 
+  heroSlider();
 } catch (error) {
   console.error(error);
 }
