d2bcdeacd43098a15fba496adefa90112bdb6e6b
rumbik13 <rembobeca@gmail.com>
2026-03-10 19:42:38 +0300

    Add files via upload

---
+++ JS-1/index.js
@@ -0,0 +1,45 @@
+// Задача 1.
+// Создайте переменные с корректными именами, чтобы сохранить следующую информацию:
+// 1. firstName – имя (строка)
+// 2. lastName – фамилия (строка)
+// 3. isStudent – является ли учеником курса (булево значение)
+
+// Задача 2.
+// 1. Объявите переменную age с числовым значением возраста студента.
+// 2. Объявите переменную currentYear и присвойте ей значение текущего года (например, 2025).
+// 3. Используя age и currentYear вычислите год рождения студента, запишите результат в переменную birthYear.
+
+// Задача 3.
+// Выведите в консоли сообщение в таком формате:
+// Меня зовут [firstName] [lastName], мне [age] лет. Я ученик/ученица курса: [isStudent].
+
+// Задача 4.
+// Какое значение будет у переменной result?
+// let a = '123';
+// let b = +'456';
+// let c = Number('789');
+// let d = Boolean(0);
+// let e = Boolean(' ');
+// let result = a + b + c + d + e;
+
+
+// 1)
+const firstName = "Abdurrahman";
+const lastName = "Eziev";
+const isStudent = true;
+
+// 2)
+let age = 13;
+let currentYear = 2026;
+let birthYear = currentYear - age;
+
+// 3)
+let information = `Меня зовут ${firstName} ${lastName}, мне ${age} лет. Я ученик курса: ${isStudent}.`;
+
+// 4)
+let a = '123';
+let b = +'456';
+let c = Number('789');
+let d = Boolean(0);
+let e = Boolean(' ');
+let result = a + b + c + d + e;//123456789falsetrue
