96d0742ddcc9639375144fbadea478c279ffca16
rumbik13 <rembobeca@gmail.com>
2026-03-16 11:34:08 +0300

    Add files via upload

--- JS-1/index.js
+++ JS-1/index.js
@@ -1,40 +1,90 @@
 // Задача 1.
-// Напишите цикл, который выводит числа от 1 до 20,
-// но пропускает те числа, которые делятся на 4 без остатка.
+// Напишите функцию calculateFinalPrice, которая принимает базовую цену товара, процент скидки и налоговую ставку. Функция должна вычислять скидку, затем прибавлять налог и возвращать итоговую цену.
 
-// Задача 2.
-// С помощью цикла for вычислите факториал числа (произведение всех чисел от 1 до заданного числа). Используйте prompt для получение начального значения.
+// Пример работы:
+// console.log(calculateFinalPrice(100, 10, 0.2)); // 108
+// console.log(calculateFinalPrice(100, 10, 0)); // 90
 
 // 1)
 
-// for (let i = 1; i <= 20; i++) {
-//   if (i % 4 === 0) {
-//     continue;
-//   }
-//   console.log(i);
+// function calculateFinalPrice(price, discount, tax) {
+//   let i = (price -= discount);
+//   let j = i * tax;
+//   return (i += j);
 // }
+
+// console.log(calculateFinalPrice(100, 10, 0.2));
+
+// Задача 2.
+// Напишите функцию checkAccess, которая принимает имя пользователя и пароль. Если имя пользователя равно "admin" и пароль равен "123456", функция должна возвращать строку "Доступ разрешен", иначе — "Доступ запрещен".
 
 // 2)
 
-// let n = 3;
-// let result = 3;
-
-// for (let i = 1; i <= n; i++) {
-//     result *= i;
+// function checkAccess(user_name, password) {
+//   if (user_name == "admin" && password == 123456) {
+//     console.log("Доступ разрешен");
+//   } else {
+//     console.log("Доступ запрещен");
+//   }
+//   return (user_name, password);
 // }
 
-// console.log(result);
+// let ADMIN = "admin";
+// let PASSWORD = 123456;
+
+// console.log(checkAccess(ADMIN , PASSWORD));
+
+// Задача 3.
+// Напишите функцию getTimeOfDay, которая принимает текущее время (число от 0 до 23) и возвращает строку:
+// "Ночь" (с 0 до 5 часов),
+// "Утро" (с 6 до 11 часов),
+// "День" (с 12 до 17 часов),
+// "Вечер" (с 18 до 23 часов).
+// Если введённое значение не попадает в этот диапазон, возвращайте `"Некорректное время"`.
 
 // 3)
 
-// let board = "";
-// let row = "";
-
-// for (let i = "1"; i <= 8; i++) {
-//   for (let j = "1"; j <= 8; j++) {
-//     console.log();
+// function getTimeOfDay(time) {
+//   let dayTime;
+//   if (time < 0 && time > 23) {
+//     console.log("Некорректное время");
 //   }
+//   switch (true) {
+//     case time >= 0 && time <= 5:
+//       dayTime = "Ночь";
+//       break;
+//     case time >= 6 && time <= 11:
+//       dayTime = "Утро";
+//       break;
+//     case time >= 12 && time <= 17:
+//       dayTime = "День";
+//       break;
+//     case time >= 18 && time <= 23:
+//       dayTime = "Вечер";
+//       break;
+//   }
+//   return dayTime;
 // }
 
-// *Задача 3.
-// Выведите в консоль шахматную доску размером 8x8, где символы чередуются (например, # и пробел). Начинайте с # в первой ячейке первой строки.
+// console.log(getTimeOfDay(19))
+
+// Задача 4.
+// Напишите функцию findFirstEven, которая принимает два числа start и end и находит первое чётное число в указанном диапазоне.
+// Если чётного числа в этом диапазоне нет, функция должна вернуть "Чётных чисел нет".
+
+// 4)
+
+// function findFirstEven(start, end) {
+//   for (let i = start; i <= end; i++) {
+//     if (i % 2 === 0) {
+//       return i;
+//     }
+//   }
+//   return "Чётных чисел нет";
+// }
+
+// console.log(findFirstEven(9, 9))
+
+// Пример работы:
+// console.log(findFirstEven(1, 10)); // 2
+// console.log(findFirstEven(9, 9)); // "Чётных чисел нет"
